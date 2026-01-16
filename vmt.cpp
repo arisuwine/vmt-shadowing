4db5dae4863794331d87081eea545cb15693181f
Alexeya Ivanova <87315162+arisuwine@users.noreply.github.com>
2026-01-16 06:02:20 +0300

    Fixed RTTI

--- vmt.cpp
+++ vmt.cpp
@@ -11,9 +11,12 @@ VMTShadowing::Shadowing(void* object) : m_pObject(nullptr), m_pVftOrig(nullptr),
 	if (m_iVftSize == 0)
 		return;
 
-	m_pUserVft = new uintptr_t[m_iVftSize];
-	memcpy(m_pUserVft, m_pVftOrig, m_iVftSize * sizeof(void*));
+	m_pShadowAlloc = new uintptr_t[m_iVftSize + 1];
+	memcpy(m_pShadowAlloc, m_pVftOrig - 1, (m_iVftSize + 1) * sizeof(void*));
+
+	m_pUserVft = m_pShadowAlloc + 1;
 	*reinterpret_cast<void**>(object) = m_pUserVft;
+
 	m_bIsInit = TRUE;
 }
 
@@ -34,7 +37,7 @@ size_t VMTShadowing::GetFunctionCount() {
 		#define PAGE_EXECUTABLE (PAGE_EXECUTE | PAGE_EXECUTE_READ | PAGE_EXECUTE_READWRITE | PAGE_EXECUTE_WRITECOPY)
 		#define PAGE_SECURE (PAGE_GUARD | PAGE_NOACCESS)
 
-		if (memory_info.State != MEM_COMMIT || memory_info.Protect & PAGE_SECURE || !memory_info.Protect & PAGE_EXECUTABLE)
+		if (memory_info.State != MEM_COMMIT || memory_info.Protect & PAGE_SECURE || !(memory_info.Protect & PAGE_EXECUTABLE))
 			break;
 
 		vft_size++;
@@ -47,11 +50,12 @@ bool VMTShadowing::UnHook(size_t index) {
 	if (!m_bIsInit)
 		return FALSE;
 
-	if (index > m_iVftSize)
+	if (index >= m_iVftSize)
 		return FALSE;
 
 	if (m_OriginalFuncs.find(index) == m_OriginalFuncs.end())
 		return FALSE;
+
 
 	m_pUserVft[index] = m_OriginalFuncs[index];
 	m_OriginalFuncs.erase(index);
@@ -75,11 +79,18 @@ bool VMTShadowing::Shutdown() {
 
 	*reinterpret_cast<uintptr_t**>(m_pObject) = m_pVftOrig;
 
-	delete[] m_pUserVft;
+	m_pObject = nullptr;
+	m_pVftOrig = nullptr;
+
+	delete[] m_pShadowAlloc;
+	m_pShadowAlloc = nullptr;
+	m_pUserVft = nullptr;
+
 	m_OriginalFuncs.clear();
+	m_bIsInit = FALSE;
 	return TRUE;
 }
 
-VMTShadowing::~Shadowing() {
+VMTShadowing::~VMTShadowing() {
 	Shutdown();
 }
